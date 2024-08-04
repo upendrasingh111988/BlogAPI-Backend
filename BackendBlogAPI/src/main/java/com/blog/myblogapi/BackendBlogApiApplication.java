8b0129417d4c24c996a2a2dfbe60092bc4678ce0
upendra singh <upendra.singh111988@gmail.com>
2024-08-04 19:16:12 +0530

    initial commit

---
+++ BackendBlogAPI/src/main/java/com/blog/myblogapi/BackendBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.myblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBlogApiApplication.class, args);
+	}
+
+}
---
+++ BackendBlogAPI/src/test/java/com/blog/myblogapi/BackendBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.myblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
